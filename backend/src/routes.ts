d79dfb78e4567f2dc4677f71e0bb8e20a9e86ed1
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-06-03 18:22:19 -0300

    Add serializer for uploaded images endpoint response

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -6,7 +6,14 @@ const routes = express.Router();
 routes.get('/items', async (req, res) => {
   const items = await knex('items').select('*');
 
-  return res.json(items);
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    }
+  });
+
+  return res.json(serializedItems);
 });
 
 export default routes;
