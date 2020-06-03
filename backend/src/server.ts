c5de7fd5b7340049d9d36a96416ff7bf1a5510ed
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-06-02 21:36:33 -0300

    Add routes and database connection files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, Ecoleta!');
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server is running on port 3333');
