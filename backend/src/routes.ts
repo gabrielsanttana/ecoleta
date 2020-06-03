0adba96e104f8bfe95044a8390efd738e494a651
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-06-03 19:53:32 -0300

    Create item controller and export it to the routes file

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,25 +1,14 @@
 import express from 'express';
 
-import knex from './database/connection';
 import PointController from './controllers/PointController';
+import ItemController from './controllers/ItemController';
 
 const pointController = new PointController();
+const itemController = new ItemController();
 
 const routes = express.Router();
 
-routes.get('/items', async (request, response) => {
-  const items = await knex('items').select('*');
-
-  const serializedItems = items.map(item => {
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
-    }
-  });
-
-  return response.json(serializedItems);
-});
+routes.get('/items', itemController.index);
 
 routes.post('/points', pointController.create);
 
