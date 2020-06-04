98feca0be431b297232db4d1d569321fc08a65c4
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-06-03 23:37:34 -0300

    Install and config cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 
 import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
