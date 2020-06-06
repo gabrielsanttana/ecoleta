865b893e91fffbaa18a0b2b4ed1af97d8c5cdb92
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-06-05 22:23:12 -0300

    Add homepage structure and styles

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
-import Home from './pages/Home';
+import Home from './pages/Home/index';
 
 const Routes = () => {
   return (
