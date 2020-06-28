b7a7ebb663f5fcd06b4952233e43b0d2476596e0
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-06-28 13:15:29 -0300

    Add assets folder and .prettierrc

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello, Ecoleta!</Text>
     </View>
   );
 }
