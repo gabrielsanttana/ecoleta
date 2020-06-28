6713ac319cc46870642d9bfbbf6607d7108197ca
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-06-28 15:46:01 -0300

    Add home page styles

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,21 +1,36 @@
 import React from 'react';
-import {View, Text, Image, TextInput, TouchableOpacity} from 'react-native';
+import {View, Text, Image, ImageBackground} from 'react-native';
+import {RectButton} from 'react-native-gesture-handler';
+import {Feather as Icon} from '@expo/vector-icons';
 import styles from './styles';
 
 const Home: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Image source={require('../../assets/logo.png')} />
+    <ImageBackground
+      source={require('../../assets/home-background.png')}
+      style={styles.container}
+      imageStyle={{width: 274, height: 368}}
+    >
+      <View style={styles.main}>
+        <Image source={require('../../assets/logo.png')} />
 
-      <Text>Seu marketplace de coleta de resíduos</Text>
-      <Text>
-        Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente
-      </Text>
+        <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+        <Text style={styles.description}>
+          Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente
+        </Text>
+      </View>
 
-      <TextInput />
-      <TextInput />
-      <TouchableOpacity />
-    </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="arrow-right" color="#fff" size={24} />
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </RectButton>
+      </View>
+    </ImageBackground>
   );
 };
 
--- mobile/src/pages/Home/styles.ts
+++ mobile/src/pages/Home/styles.ts
@@ -3,9 +3,56 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 32,
+    backgroundColor: '#f0f0f5',
+  },
+
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginTop: 48,
+    color: '#322153',
+    maxWidth: 270,
+  },
+
+  description: {
+    fontSize: 16,
+    marginTop: 16,
+    color: '#6C6C80',
+    maxWidth: 270,
+    lineHeight: 24,
+  },
+
+  footer: {},
+
+  button: {
+    backgroundColor: '#34CB79',
+    height: 60,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 8,
+    overflow: 'hidden',
+  },
+
+  buttonIcon: {
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    height: 60,
+    width: 60,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+
+  buttonText: {
+    color: '#fff',
+    flex: 1,
+    textAlign: 'center',
+    fontSize: 16,
   },
 });
 
