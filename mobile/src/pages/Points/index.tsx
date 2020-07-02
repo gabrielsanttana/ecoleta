807381d935b8ebca2ba56f3612e78303823505ab
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-07-02 20:32:33 -0300

    Add function to navigate back to home on Points page

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -3,13 +3,20 @@ import {View, Text} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {Feather as Icon} from '@expo/vector-icons';
 import MapView from 'react-native-maps';
+import {useNavigation} from '@react-navigation/native';
 import styles from './styles';
 
 const Points: React.FC = () => {
+  const navigator = useNavigation();
+
+  const navigateToHome = () => {
+    return navigator.navigate('Home');
+  };
+
   return (
     <>
       <View style={styles.container}>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={navigateToHome}>
           <Icon name="arrow-left" size={24} color="#34CB79" />
         </TouchableOpacity>
 
