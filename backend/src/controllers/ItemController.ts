d08c90ae39b17e26329b377c089b063809dc46e4
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-07-02 21:18:51 -0300

    Install dotenv and add environment variable for IP address to call the API on mobile

--- backend/src/controllers/ItemController.ts
+++ backend/src/controllers/ItemController.ts
@@ -1,16 +1,19 @@
 import {Request, Response} from 'express';
 import knex from '../database/connection';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 class ItemController {
   async index(request: Request, response: Response) {
     const items = await knex('items').select('*');
 
-    const serializedItems = items.map(item => {
+    const serializedItems = items.map((item) => {
       return {
         id: item.id,
         title: item.title,
-        image_url: `http://localhost:3333/uploads/${item.image}`,
-      }
+        image_url: `http://${process.env.API_IP_ADDRESS}:3333/uploads/${item.image}`,
+      };
     });
 
     return response.json(serializedItems);
