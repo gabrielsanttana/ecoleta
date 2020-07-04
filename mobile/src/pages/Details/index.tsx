201d93dc998dd00746d259106c105fe7f77e60a2
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-07-04 15:52:06 -0300

    Add Details page initial styling

--- mobile/src/pages/Details/index.tsx
+++ mobile/src/pages/Details/index.tsx
@@ -6,36 +6,38 @@ import styles from './styles';
 
 const Details: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.goBackButton}>
-        <Icon name="arrow-left" size={16} color="#34CB79" />
-      </TouchableOpacity>
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.goBackButton}>
+          <Icon name="arrow-left" size={24} color="#34CB79" />
+        </TouchableOpacity>
 
-      <Image
-        style={styles.image}
-        source={{
-          uri:
-            'https://www.tce.sp.gov.br/sites/default/files/styles/max_800x800/public/noticias/coleta%20seletivaaa.png',
-        }}
-      />
+        <Image
+          style={styles.image}
+          source={{
+            uri:
+              'https://www.tce.sp.gov.br/sites/default/files/styles/max_800x800/public/noticias/coleta%20seletivaaa.png',
+          }}
+        />
 
-      <Text style={styles.pointTitle}>Mercado do Carlão</Text>
-      <Text style={styles.pointItems}>Lâmpadas, Óleo de Cozinha</Text>
+        <Text style={styles.pointTitle}>Mercado do Carlão</Text>
+        <Text style={styles.pointItems}>Lâmpadas, Óleo de Cozinha</Text>
 
-      <Text style={styles.pointAddressTitle}>Endereço</Text>
-      <Text style={styles.pointAddressDescription}>Americana, São Paulo</Text>
+        <Text style={styles.pointAddressTitle}>Endereço</Text>
+        <Text style={styles.pointAddressDescription}>Americana, São Paulo</Text>
+      </View>
 
       <View style={styles.footer}>
-        <RectButton style={styles.contactButton}>
+        <TouchableOpacity style={styles.contactButton}>
           <Icon name="whatsapp" size={16} color="#fff" />
           <Text style={styles.contactButtonText}>Whatsapp</Text>
-        </RectButton>
-        <RectButton style={styles.contactButton}>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.contactButton}>
           <Icon name="envelope" size={16} color="#fff" />
           <Text style={styles.contactButtonText}>E-mail</Text>
-        </RectButton>
+        </TouchableOpacity>
       </View>
-    </View>
+    </>
   );
 };
 
--- mobile/src/pages/Details/styles.ts
+++ mobile/src/pages/Details/styles.ts
@@ -1,28 +1,76 @@
 import {StyleSheet} from 'react-native';
+import Constants from 'expo-constants';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight + 32,
+    paddingHorizontal: 24,
   },
 
-  goBackButton: {},
+  goBackButton: {
+    marginBottom: 24,
+  },
 
-  image: {},
+  image: {
+    width: '100%',
+    height: 130,
+    borderRadius: 10,
+    resizeMode: 'cover',
+  },
 
-  pointTitle: {},
+  pointTitle: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginTop: 24,
+    color: '#322153',
+  },
 
-  pointItems: {},
+  pointItems: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: '#6C6C80',
+    marginBottom: 4,
+  },
 
-  pointAddressTitle: {},
+  pointAddressTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 24,
+    color: '#322153',
+  },
 
-  pointAddressDescription: {},
+  pointAddressDescription: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: '#6C6C80',
+  },
 
-  footer: {},
+  footer: {
+    marginTop: 60,
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderTopColor: '#6C6C80',
+    paddingVertical: 20,
+    paddingHorizontal: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
 
-  contactButton: {},
+  contactButton: {
+    width: '48%',
+    height: 50,
+    backgroundColor: '#34CB79',
+    borderRadius: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
-  contactButtonText: {},
+  contactButtonText: {
+    fontSize: 16,
+    color: '#fff',
+    marginLeft: 6,
+  },
 });
 
 export default styles;
